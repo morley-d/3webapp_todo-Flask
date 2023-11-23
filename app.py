1b5983a8455b0c2be4635f9b3c22ea737759baec
Andrey <21elektro87@gmail.com>
2023-11-23 17:54:11 +0300

    create project files

--- app.py
+++ app.py
@@ -1,1 +1,6 @@
 from flask import Flask
+
+
+
+if __name__ == "__main__":
+    pass
---
+++ todo/routes.py
@@ -0,0 +1,17 @@
+from flask import Flask, request, render_template, url_for, redirect
+
+
+def create_app():
+    app = Flask(__name__)
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    return app
+
+
+app = create_app()
+
+
+@app.get('/')
+def home():
+    pass
