5f5c1672606d94588707b38461152354d374baf7
morley-d <103437424+morley-d@users.noreply.github.com>
2023-11-23 18:07:01 +0300

    init app for db

--- todo/routes.py
+++ todo/routes.py
@@ -1,11 +1,12 @@
 from flask import Flask, request, render_template, url_for, redirect
+from todo.models import ToDo, db
 
 
 def create_app():
     app = Flask(__name__)
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
+    db.init_app(app)
     return app
 
 
